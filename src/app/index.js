f4f4a643acf85fde617fae4117c84d51ca436ff5
Trisky <sebastian@styrsky.com.ar>
2016-11-08 15:34:09 -0300

    first day of work (reverted from commit 2b31e82910af1a3cee1ef7e9c16e50e6a020eb7a)

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,7 @@
 import {routes} from './routes';
 import {ApiService} from './services';
-import {Controllers} from './Controllers';
 
 
 angular.module('App', ['ngRoute'])
     .config(routes)
     .service('ApiService', ApiService);
-    
-
--- src/index.js
+++ src/index.js
@@ -2,25 +2,5 @@ import angular from 'angular';
 
 import './app';
 
+
 angular.bootstrap(document, ['App']);
-
-var moviesController = angular.controller('ArtistCtrl', ['$cookies', '$scope', function ($scope) {
-
-    var searchUrl = "https://api.spotify.com/v1/search?q="
-
-    $scope.searchArtist = function () {
-        Console.log("asd");
-        var searchTerm = $scope.searchInput;
-        searchTerm.replace(' ', '+');
-        $http({
-            method: 'GET',
-            url: searchUrl + searchTerm + "*",
-        }).then(function successCallback(response) {
-            console.log(response);
-        }, function errorCallback(response) {
-            alert("error");
-        });
-    };
-
-
-}]);
