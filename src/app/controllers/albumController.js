27a3621cf89754cf88e79b512cd6cddcffea1105
Trisky <sebastian@styrsky.com.ar>
2016-11-21 17:34:44 -0300

    working favorites

--- src/app/controllers/albumController.js
+++ src/app/controllers/albumController.js
@@ -3,21 +3,27 @@ export function albumController($scope, $http, $routeParams, ApiService, favorit
     var searchTerm = 'https://api.spotify.com/v1/albums/' + albumID + '/tracks';
     $scope.albumImage = ApiService.selectedAlbum.images[2].url;
     $scope.albumName = ApiService.selectedAlbum.name;
+    console.log(ApiService.selectedAlbum);
 
     ApiService.getInfo(searchTerm, this.showAlbums)
         .then(data => { this.showTracks(data); });
 
-    this.showTracks = function(data) {
+    this.showTracks = function (data) {
         $scope.trackList = data.items;
         console.log(data);
     };
 
-    $scope.toggleFavorite = function(item) { //recieves a track item
-        favoritesService.toggle(item);
+    $scope.toggleFavorite = function (item) { //recieves a track item
+        favoritesService.toggle(item,ApiService.selectedAlbum);
     };
-    $scope.playSong = function(trackId) {
+    $scope.playSong = function (trackId) {
         $scope.previewUrl = $scope.trackList[trackId].preview_url;
         //mostrar modal con el player. TODO
     };
+    $scope.favorited = function (id) {
+        return favoritesService.isFavorited(id);
+    };
+    
+
 }
 
--- src/app/services/favoritesService.js
+++ src/app/services/favoritesService.js
@@ -2,38 +2,58 @@ export function favoritesService($localStorage) {
 
     $localStorage.favs = $localStorage.favs || [];
 
-    this.toggle = function(item) {
+    this.toggle = function (item, album) {
         var track = $localStorage.favs.find(this.getTrackById(item));
         // console.log(track);
         if (!track)
-            $localStorage.favs.push(item);
+            this.addFav(item, album);
+
         else
             this.removeTrack(item);
         console.log($localStorage);
     };
 
-    this.getFavorites = function() {
+
+    this.getFavorites = function () {
         return $localStorage.favs;
     };
 
 
-    this.getTrackById = function(item) {
-        return function(element) {
+    this.getTrackById = function (item) {
+        return function (element) {
             return element.id === item.id;
         };
     };
-    this.removeTrack = function(item) {
+    this.addFav = function (item, album) {
+        var track = new Track(item, album);
+        $localStorage.favs.push(track);
+    };
+
+    this.isFavorited = function (id) {
+        var item = {};
+        item.id = id;
+        var track = $localStorage.favs.find(this.getTrackById(item));
+        if (track != undefined) return true;
+        else return false;
+
+    };
+    this.removeTrack = function (item) {
         var favs = $localStorage.favs;
-        var index = favs.indexOf(item);
-        if (index > -1) {
-            favs.splice(index, 1);
+
+        for (var i = 0; i < favs.length; i++) {
+            if (favs[i].id == item.id) {
+                favs.splice(i, 1);
+                return;
+            }
         }
     };
 
-    function Track(item) {
+    function Track(item, album) {
         this.artist = item.artists[0].name;
         this.name = item.name;
-        this.href = item.href;
+        this.href = item.preview_url;
         this.id = item.id;
+        this.albumName = album.name;
+        this.image = album.images[2].url;
     }
 }
